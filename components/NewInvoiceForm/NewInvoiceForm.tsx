b2367f42fca259641275fd09043d593b2d55d25c
bmatlak <bartosz.matlak@op.pl>
2022-02-18 12:33:08 +0100

    refactoring homepage and adding basic form

---
+++ components/NewInvoiceForm/NewInvoiceForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextField } from "@mui/material";
+type Props = {};
+
+const NewInvoiceForm = (props: Props) => {
+  return (
+    <form action="">
+      <TextField variant="outlined" />
+    </form>
+  );
+};
+export default NewInvoiceForm;
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,56 +9,56 @@ import Image from "next/image";
 const Home: NextPage = () => {
   const invoices = useSelector((state: RootState) => state.invoices.invoices);
   console.log(invoices);
-  return (
-    <Box sx={{ padding: 2 }}>
-      <InvoicesAction />
-      <List>
-        {invoices.length !== 0 ? (
-          invoices.map(
-            (invoice: {
-              id: string;
-              paymentDue: string;
-              clientName: string;
-              status: string;
-              total: number;
-            }) => (
-              <Invoice
-                key={invoice.id}
-                id={invoice.id}
-                paymentDue={invoice.paymentDue}
-                clientName={invoice.clientName}
-                status={invoice.status}
-                total={invoice.total}
-              />
-            )
-          )
-        ) : (
-          <Box
-            sx={{
-              display: "flex",
-              width: "100%",
-              height: "100%",
-              flexDirection: "column",
-              alignItems: "center",
-              justifyContent: "center",
-              textAlign: "center",
-            }}
-          >
-            <Image src={EmptyInvoicesIMG} alt="no invoices yet" />
-            <Typography sx={{ fontWeight: "bold" }} variant="h4">
-              There is nothing here
-            </Typography>
-            <Typography color="text.secondary">
-              Create an invoice by clicking the{" "}
-              <Typography sx={{ fontWeight: "bold" }} component="span">
-                New
-              </Typography>{" "}
-              button and get started
-            </Typography>
-          </Box>
-        )}
-      </List>
+  const noInvoicesInfo = (
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        textAlign: "center",
+      }}
+    >
+      <Image src={EmptyInvoicesIMG} alt="no invoices yet" />
+      <Typography sx={{ fontWeight: "bold" }} variant="h4">
+        There is nothing here
+      </Typography>
+      <Typography color="text.secondary">
+        Create an invoice by clicking the{" "}
+        <Typography sx={{ fontWeight: "bold" }} component="span">
+          New
+        </Typography>{" "}
+        button and get started
+      </Typography>
     </Box>
+  );
+  const renderedInvoices = invoices.map(
+    (invoice: {
+      id: string;
+      paymentDue: string;
+      clientName: string;
+      status: string;
+      total: number;
+    }) => (
+      <Invoice
+        key={invoice.id}
+        id={invoice.id}
+        paymentDue={invoice.paymentDue}
+        clientName={invoice.clientName}
+        status={invoice.status}
+        total={invoice.total}
+      />
+    )
+  );
+  return (
+    <>
+      <Box sx={{ padding: 2 }}>
+        <InvoicesAction />
+        <List>{invoices.length !== 0 ? renderedInvoices : noInvoicesInfo}</List>
+      </Box>
+    </>
   );
 };
 
