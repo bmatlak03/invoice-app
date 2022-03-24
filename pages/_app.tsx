550164b7e16aebb241a762acd0c1255f79e45126
bmatlak <bartosz.matlak@op.pl>
2022-03-25 00:00:33 +0100

    displaying loading state on route change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,20 @@
 import type { AppProps } from "next/app";
+import Router from "next/router";
 import { Provider } from "react-redux";
 import store from "../store/index";
 import Layout from "../components/UI/Layout/Layout";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
+import "../css/nprogress.css";
+import NProgress from "nprogress";
+
+NProgress.configure({ showSpinner: false });
+const handleRouteChange = () => NProgress.start();
+const handleRouteComplete = () => NProgress.done();
+
+Router.events.on("routeChangeStart", handleRouteChange);
+Router.events.on("routeChangeComplete", handleRouteComplete);
+Router.events.on("routeChangeError", handleRouteComplete);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
