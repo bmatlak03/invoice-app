8858f03be29b7742fe260c6fc37c0233d782718c
bmatlak <bartosz.matlak@op.pl>
2022-04-10 18:54:50 +0200

    refactoring invoiceId page

--- pages/[invoiceId]/index.tsx
+++ pages/[invoiceId]/index.tsx
@@ -1,12 +1,10 @@
-import { ObjectId } from "mongodb";
-import { GetStaticProps, GetStaticPaths } from "next";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { invoicesActions } from "../../store/invoices-slice";
 import { InvoiceType } from "../../store/invoices-slice";
 import InvoiceDetails from "../../components/InvoiceDetails/InvoiceDetails";
-import { useRouter } from "next/router";
 import { getSession } from "next-auth/react";
 import { connectToDatabase } from "../../lib/db";
 type Props = {
@@ -15,16 +13,9 @@ type Props = {
 
 const InvoiceDetailsPage = ({ invoiceData }: Props) => {
   const dispatch = useDispatch();
-  const router = useRouter();
   useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        router.replace("/auth");
-      } else {
-        dispatch(invoicesActions.insertFetchedInvoice(invoiceData));
-      }
-    });
-  }, [router, dispatch, invoiceData]);
+    dispatch(invoicesActions.insertFetchedInvoice(invoiceData));
+  }, [dispatch, invoiceData]);
   return (
     <>
       <Head>
@@ -34,50 +25,55 @@ const InvoiceDetailsPage = ({ invoiceData }: Props) => {
     </>
   );
 };
-export const getStaticPaths: GetStaticPaths = async () => {
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession({ req: context.req });
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
   const client = await connectToDatabase();
-  const db = client.db();
-
-  const invoicesCollection = db.collection("invoices");
-  const id: any = { _id: 1 };
-  const invoices = await invoicesCollection.find({}, id).toArray();
-
-  client.close();
-
-  return {
-    fallback: "blocking",
-    paths: invoices.map((invoice) => ({
-      params: { invoiceId: invoice._id.toString() },
-    })),
-  };
-};
-
-export const getStaticProps: GetStaticProps = async (context) => {
   const id: any = context?.params?.invoiceId;
-
-  const client = await connectToDatabase();
   const db = client.db();
-  const invoicesCollection = db.collection("invoices");
-  const selectedInvoice = await invoicesCollection.findOne({
-    _id: new ObjectId(id),
-  });
+
+  const user = await db
+    .collection("users")
+    .find({ email: session?.user?.email })
+    .toArray();
+  const invoices = user[0].invoices;
+  const selectedInvoice: InvoiceType[] = invoices.filter(
+    (invoice: InvoiceType) => invoice.id === id
+  );
+  if (selectedInvoice.length === 0) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   client.close();
 
   return {
     props: {
       invoiceData: {
-        id: selectedInvoice?._id.toString(),
-        status: selectedInvoice?.status,
-        description: selectedInvoice?.description,
-        createdAt: selectedInvoice?.createdAt,
-        paymentDue: selectedInvoice?.paymentDue,
-        paymentTerms: selectedInvoice?.paymentTerms,
-        clientName: selectedInvoice?.clientName,
-        clientEmail: selectedInvoice?.clientEmail,
-        senderAddress: selectedInvoice?.senderAddress,
-        clientAddress: selectedInvoice?.clientAddress,
-        items: selectedInvoice?.items,
-        total: selectedInvoice?.total,
+        id: selectedInvoice[0].id,
+        status: selectedInvoice[0].status,
+        description: selectedInvoice[0].description,
+        createdAt: selectedInvoice[0].createdAt,
+        paymentDue: selectedInvoice[0].paymentDue,
+        paymentTerms: selectedInvoice[0].paymentTerms,
+        clientName: selectedInvoice[0].clientName,
+        clientEmail: selectedInvoice[0].clientEmail,
+        senderAddress: selectedInvoice[0].senderAddress,
+        clientAddress: selectedInvoice[0].clientAddress,
+        items: selectedInvoice[0].items,
+        total: selectedInvoice[0].total,
       },
     },
   };
